6265600ca7eee9794b316399b75077c8714dd988
Jessica Owens <jessicadanaowens@gmail.com>
2014-08-25 10:57:44 -0600

    added more tests

--- spec/features/movies_spec.rb
+++ spec/features/movies_spec.rb
@@ -14,6 +14,32 @@ feature "User can add a movie" do
 
     expect(page).to have_content ("Moonstruck")
   end
+
+  scenario "unsuccessfully" do
+    visit root_url
+
+    click_on "Add Movie"
+
+    click_on "Add Movie"
+
+    expect(page).to have_content ("Name can't be blank")
+    expect(page).to have_content ("Year can't be blank")
+    expect(page).to have_content ("Year is not a number")
+    expect(page).to have_content ("Synopsis can't be blank")
+  end
+
+  scenario "user must enter an number in date field or get error" do
+    visit root_url
+
+    click_on "Add Movie"
+
+    fill_in "Year", :with => "hello"
+
+    click_on "Add Movie"
+
+    expect(page).to have_content ("Year is not a number")
+
+  end
 end
 
 
